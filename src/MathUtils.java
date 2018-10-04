e41dff1d8f502f6f0434e4142691c34f6065bfa9
agarw167 <agarw167@purdue.edu>
2018-10-04 13:01:39 -0400

    test commit

--- src/MathUtils.java
+++ src/MathUtils.java
@@ -8,7 +8,8 @@ public class MathUtils {
     public static double divideNumbers(int dividend, int divisor) {
 
         /* Do some casual division ... Super easy ... Shouldn't be any problems here */
-        double res = dividend / divisor;
+        double res = (double)dividend /(double) divisor;
+
         /* Return the DEFINITELY CORRECT result */
         return res;
     }
@@ -32,13 +33,13 @@ public class MathUtils {
         /* Multiply the result by the base exp times
             Ex: base = 2, exp = 2
                --> res = 2, then res = 4 */
-        for (int i = 0; i < exp; i++) {
-           res= i*base;
+        for (int i = 0; i < exp - 1; i++) {
+           res*=base;
         }
 
         /* If it's a negative exponent, we should invert it! */
         if (isNegative) {
-            double dividedRes = divideNumbers(1, res);
+            double dividedRes = divideNumbers(1,res);
             return dividedRes;
         }
         /* Otherwise, we are safe to just return the result */
